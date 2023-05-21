311e8622e754a477281aff66aac317cef532694d
RitulD <ritul.deshmukh@ula.app>
2023-05-21 21:57:57 +0530

    cluster map

--- models/campground.js
+++ models/campground.js
@@ -8,9 +8,9 @@ const ImageSchema = new Schema({
     filename: String
 });
 
-ImageSchema.virtual('thumbnail').get(function () {
-    return this.url.replace('/upload', '/upload/w_200');
-});
+
+
+const opts = { toJSON: { virtuals: true } };
 
 const campGroundSchema = new Schema({
     title: String,
@@ -39,6 +39,15 @@ const campGroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+campGroundSchema.virtual('properties.popUpMarkup').get(function () {
+    // return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong><p>${this.description.substring(0, 20)}...</p>`
+    return `<strong>${this.title} </strong>`;
 });
 
 campGroundSchema.post('findOneAndDelete', async function (doc) {
